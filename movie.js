3ab95f081e55ee99144753d812bac482bf8b798e
Eshita Sharma <125384653+E14sharma@users.noreply.github.com>
2023-08-13 17:43:44 +0530

    Update movie.js

--- movie.js
+++ movie.js
@@ -1,108 +1,156 @@
-  let pageNumber;
+const API_KEY = '4309e7869e5a6f7164f1fff7195ee87b';
+const API_URL = `https://api.themoviedb.org/3/discover/movie?include_adult=false&language=en-US&page=${pageNumber}&sort_by=$`;
+const POSTER_BASE_URL = 'https://image.tmdb.org/t/p/original/';
+
+const movieList = document.getElementById('movie-list');
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+const sortByDateButton = document.getElementById('sortByDate');
+const sortByRatingButton = document.getElementById('sortByRating');
+const allTab = document.getElementById('allTab');
+const favoritesTab = document.getElementById('favoritesTab');
+const prevPageButton = document.getElementById('prevPage');
+const nextPageButton = document.getElementById('nextPage');
+const currentPageSpan = document.getElementById('currentPage');
+const favoriteIconClass = 'fas fa-heart';
+
+let currentPage = 1;
 let totalPages = 0;
-let currentState = "desc";
+let moviesData = [];
+let favorites = [];
 
-const movieListSection = document.querySelector("#movie-list");
-const nextBtn = document.querySelector("#next");
-const backBtn = document.querySelector("#prev");
-const pageNumberContainer = document.querySelector("#page-no");
-const ratingToggle = document.querySelector("#rating-toggle");
-
-const SORT_ASC_TEXT = "Sort by rating descending";
-const SORT_DESC_TEXT = "Sort by rating ascending";
-
-const SORT_ASC = "popularity.asc";
-const SORT_DESC = "popularity.desc";
-
-function addNavigationButtons() {
-  nextBtn.addEventListener("click", () => {
-    if (pageNumber < totalPages) {
-      pageNumber++;
-      showMovies(pageNumber, currentState);
+// Fetch movies data from API
+async function fetchMovies(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        totalPages = data.total_pages;
+        moviesData = data.results;
+        renderMovies();
+    } catch (error) {
+        console.error('Error fetching movies:', error);
     }
-  });
+}
 
-  backBtn.addEventListener("click", () => {
-    if (pageNumber > 1) {
-      pageNumber--;
-      showMovies(pageNumber, currentState);
+// Render movies in cards
+function renderMovies() {
+    movieList.innerHTML = '';
+
+    for (let movie of moviesData) {
+        const movieCard = createMovieCard(movie);
+        movieList.appendChild(movieCard);
     }
-  });
+
+    updatePaginationButtons();
 }
 
-function addPopularityButton() {
-  ratingToggle.addEventListener("click", (e) => {
-    currentState = currentState === "desc" ? "asc" : "desc";
-    pageNumber = 1;
-    showMovies(pageNumber, currentState);
-    e.target.innerText =
-      currentState === "desc" ? SORT_DESC_TEXT : SORT_ASC_TEXT;
-  });
+// Create a movie card element
+function createMovieCard(movie) {
+    const movieCard = document.createElement('div');
+    movieCard.classList.add('movie-card');
+
+    const posterURL = POSTER_BASE_URL + movie.poster_path;
+
+    const content = `
+        <img src="${posterURL}" alt="${movie.title}" class="movie-poster">
+        <h3 class="movie-title">${movie.title}</h3>
+        <p class="vote-count">Vote Count: ${movie.vote_count}</p>
+        <p class="vote-average">Vote Average: ${movie.vote_average}</p>
+        <i class="${favoriteIconClass} favorite-icon" data-movie-id="${movie.id}"></i>
+    `;
+
+    movieCard.innerHTML = content;
+
+    const favoriteIcon = movieCard.querySelector('.favorite-icon');
+    favoriteIcon.addEventListener('click', toggleFavorite);
+
+    return movieCard;
 }
 
-async function showMovies(pageNumber = 1, sort_by = "desc") {
-  movieListSection.innerText = "";
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZjEwMjIzNGM0YzcwZjZhMDRmNWIyOTFlN2ZjMTU4YyIsInN1YiI6IjY0ZDNjMGI3MDIxY2VlMDExYzhmMTYzNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qRQZAy5zW4VQAn64hrz_hC4qTOOZH8Xfwx9vfMIEBzE",
-    },
-  };
-  let response;
+// Toggle favorite status
+function toggleFavorite(event) {
+    const movieId = parseInt(event.target.dataset.movieId);
+    if (favorites.includes(movieId)) {
+        favorites = favorites.filter(id => id !== movieId);
+    } else {
+        favorites.push(movieId);
+    }
 
-  response = await fetch(
-    `https://api.themoviedb.org/3/discover/movie?include_adult=false&language=en-US&page=${pageNumber}&sort_by=${
-      sort_by === "asc" ? SORT_ASC : SORT_DESC
-    }`,
-    options
-  );
-
-  const json = await response.json();
-  totalPages = json.total_pages;
-  const movieList = json.results;
-  for (let movie of movieList) {
-    // movie section
-    const movieTitle = document.createElement("h2");
-    movieTitle.innerText = movie.title;
-    const rating = document.createElement("p");
-    rating.innerText = movie.vote_average;
-    const movieDetails = document.createElement("section");
-    movieDetails.appendChild(movieTitle);
-    movieDetails.appendChild(rating);
-    movieDetails.classList.add("movie-details");
-    // image/ banner
-    let banner = document.createElement("img");
-    banner.src = `https://image.tmdb.org/t/p/original/${movie.backdrop_path}`;
-    banner.classList.add("movie-poster");
-    // footer
-    const footer = document.createElement("footer");
-    const date = document.createElement("p");
-    date.innerText = `date ${movie.release_date}`;
-    const heart = document.createElement("i");
-    heart.classList.add("fa-regular", "fa-heart", "like");
-    footer.appendChild(date);
-    footer.appendChild(heart);
-    // parent
-    const movieElement = document.createElement("article");
-    movieElement.classList.add("movie");
-    movieElement.appendChild(banner);
-    movieElement.appendChild(movieTitle);
-    movieElement.appendChild(footer);
-    movieListSection.appendChild(movieElement);
-    pageNumberContainer.innerText = pageNumber;
-  }
-
-  //   movieList
+    event.target.classList.toggle('selected');
+    saveFavoritesToLocalStorage();
 }
 
-async function init() {
-  pageNumber = 1;
-  await showMovies(1);
-  addNavigationButtons();
-  addPopularityButton();
+// Save favorites to local storage
+function saveFavoritesToLocalStorage() {
+    localStorage.setItem('favorites', JSON.stringify(favorites));
 }
 
-// application code
+// Load favorites from local storage
+function loadFavoritesFromLocalStorage() {
+    const favoritesData = localStorage.getItem('favorites');
+    if (favoritesData) {
+        favorites = JSON.parse(favoritesData);
+    }
+}
+
+// Sort movies by date or rating
+function sortMovies(sortBy) {
+    moviesData.sort((a, b) => {
+        if (sortBy === 'date') {
+            return new Date(a.release_date) - new Date(b.release_date);
+        } else if (sortBy === 'rating') {
+            return b.vote_average - a.vote_average;
+        }
+    });
+
+    renderMovies();
+}
+
+// Render favorite movies
+function renderFavorites() {
+    const favoriteMovies = moviesData.filter(movie => favorites.includes(movie.id));
+    movieList.innerHTML = '';
+
+    for (let movie of favoriteMovies) {
+        const movieCard = createMovieCard(movie);
+        movieList.appendChild(movieCard);
+    }
+}
+
+// Change current page
+function changePage(page) {
+    if (page >= 1 && page <= totalPages) {
+        currentPage = page;
+        fetchMovies(`${API_URL}&page=${currentPage}`);
+    }
+}
+
+// Update pagination buttons
+function updatePaginationButtons() {
+    prevPageButton.disabled = currentPage === 1;
+    nextPageButton.disabled = currentPage === totalPages;
+    currentPageSpan.textContent = currentPage;
+}
+
+// Initialize the app
+function init() {
+    loadFavoritesFromLocalStorage();
+    fetchMovies(API_URL);
+}
+
+// Event listeners
+searchButton.addEventListener('click', () => {
+    const query = searchInput.value.trim();
+    if (query !== '') {
+        fetchMovies(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`);
+    }
+});
+sortByDateButton.addEventListener('click', () => sortMovies('date'));
+sortByRatingButton.addEventListener('click', () => sortMovies('rating'));
+allTab.addEventListener('click', () => fetchMovies(API_URL));
+favoritesTab.addEventListener('click', renderFavorites);
+prevPageButton.addEventListener('click', () => changePage(currentPage - 1));
+nextPageButton.addEventListener('click', () => changePage(currentPage + 1));
+
+// Initialize the app
 init();
